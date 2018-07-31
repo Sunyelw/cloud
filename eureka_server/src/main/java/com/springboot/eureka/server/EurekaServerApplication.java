f25a896fb1daf0e0555ddc751bbaf10ebf252b6d
huangyang <huangyang3@asiainfo.com>
2018-07-31 23:31:14 +0800

    init eureka

---
+++ eureka_server/src/main/java/com/springboot/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.springboot.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Sunyelw
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
