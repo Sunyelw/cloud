fc68b6da49655e92484c8eabecd617fdd3da4aea
huangyang <huangyang3@asiainfo.com>
2018-08-07 23:04:17 +0800

    add schedule

---
+++ springcloud-config-client/src/main/java/com/springcloud/config/client/SchedultTask.java
@@ -0,0 +1,44 @@
+package com.springcloud.config.client;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * 项目名称:   pinkstone
+ * 包:        com.springcloud.config.client
+ * 类名称:     AA
+ * 类描述:     ss
+ * 创建人:     huangyang
+ * 创建时间:   2018/8/7 20:22
+ */
+@Component
+public class SchedultTask {
+
+	@Scheduled(cron = "0/5 * *  * * ?")
+	public void tasks() {
+		System.out.println(new Date().toLocaleString() + "AA");
+	}
+
+	/**
+	 * 6位随机二维码 纯数字
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Set<Integer> codes = new HashSet<>();
+		Random random = new Random();
+		for (int i = 0; i < i + 1; i++) {
+			if (codes.size() == 6) {
+				break;
+			} else {
+				codes.add(random.nextInt(10));
+			}
+		}
+		System.out.println(StringUtils.join(codes.toArray()));
+	}
+}
--- springcloud-config-client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
+++ springcloud-config-client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
@@ -3,15 +3,19 @@ package com.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Date;
 
 /**
  * @author Sunyelw
  */
 @SpringBootApplication
+@EnableScheduling
 @RestController
 public class SpringcloudConfigClientApplication {
 
