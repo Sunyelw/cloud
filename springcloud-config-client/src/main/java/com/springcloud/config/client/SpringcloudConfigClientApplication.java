b6871d4f41629b1b8b926da882432ee2dda82426
huangyang <huangyang3@asiainfo.com>
2018-08-11 18:33:40 +0800

    eureka+config

--- springcloud-config-client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
+++ springcloud-config-client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
@@ -3,7 +3,9 @@ package com.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,15 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
 @RestController
 public class SpringcloudConfigClientApplication {
 
 	@Value("${eureka.server.port}")
 	private String config;
+	@Value("${eureka.client.serviceUrl.defaultZone}")
+	private String eureka;
 
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello . " + config;
+	}
+
+	@RequestMapping("/eureka")
+	public String getEureka() {
+		return "Hei, " + eureka;
 	}
 
 	public static void main(String[] args) {
