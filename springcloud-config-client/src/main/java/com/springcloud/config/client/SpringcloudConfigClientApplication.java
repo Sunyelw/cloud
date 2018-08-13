6c98683036cfa9598c856e1a1feabd4f49d4eb56
huangyang <yellowsunsna@163.com>
2018-08-13 21:24:57 +0800

    resolve eureka server program

--- springcloud-config-client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
+++ springcloud-config-client/src/main/java/com/springcloud/config/client/SpringcloudConfigClientApplication.java
@@ -3,9 +3,7 @@ package com.springcloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @EnableScheduling
-@EnableEurekaClient
 @RestController
 public class SpringcloudConfigClientApplication {
 
-	@Value("${eureka.server.port}")
+	@Value("${server.port}")
 	private String config;
 	@Value("${eureka.client.serviceUrl.defaultZone}")
 	private String eureka;
--- springcloud-config-server/src/main/java/com/springcloud/config/server/SpringcloudConfigServerApplication.java
+++ springcloud-config-server/src/main/java/com/springcloud/config/server/SpringcloudConfigServerApplication.java
@@ -3,12 +3,14 @@ package com.springcloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Sunyelw
  */
 @Configuration
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class SpringcloudConfigServerApplication {
