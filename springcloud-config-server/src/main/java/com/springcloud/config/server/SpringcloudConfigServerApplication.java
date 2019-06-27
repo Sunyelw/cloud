ce9d417c9a3756764b771d7550f49e1724e2a8b5
huangyang3 <huangyang3@asiainfo.com>
2019-06-27 14:46:24 +0800

    configserver 配置优化

--- springcloud-config-server/src/main/java/com/springcloud/config/server/SpringcloudConfigServerApplication.java
+++ springcloud-config-server/src/main/java/com/springcloud/config/server/SpringcloudConfigServerApplication.java
@@ -3,14 +3,10 @@ package com.springcloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Sunyelw
  */
-//@Configuration
-//@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
 public class SpringcloudConfigServerApplication {
